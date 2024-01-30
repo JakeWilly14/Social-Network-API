8283ab52260ffefd0ce00ac2948d7a8dcc8ef03b
Jakewilly14 <jakewilly14@gmail.com>
2024-01-30 01:51:19 -0600

    User.js Model / Packages Install

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+const assignmentSchema = require('./Assignment');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: `/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/`,
+    },
+    thoughts: [thoughtSchema],
+    friends: [userSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virutal('friendCount').get(function () {
+  return `${this.username} ${this.email}`
+});
+
+const User = model('user', userSchema);
+
+model.exports = User;
